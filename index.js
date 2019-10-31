b835331e5e7785b4f46dd67253c0b45c72a05f39
ricksuggs <suggsra@gmail.com>
2019-10-31 15:53:05 -0500

    Updates

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const five = require('johnny-five');
 const PubNub = require('pubnub');
 const uuid = PubNub.generateUUID();
 const pubnub = new PubNub({
-  subscribeKey: 'sub-c-fa806a90-f675-11e9-8dd7-ca99873d233c',
-  publishKey: 'pub-c-7c75b730-15fd-4037-9c5d-81d072993831',
+  subscribeKey: 'sub-demo',
+  publishKey: 'pub-demo',
   uuid: uuid
 });
 
